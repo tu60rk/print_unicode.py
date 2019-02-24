23eef3d86e8d034d8b78128d37634851a264e0e4
tu60rk <tu60rk@yandex.ru>
2019-02-24 13:19:36 +0300

    Update print_unicode.py

--- print_unicode.py
+++ print_unicode.py
@@ -1,17 +1,10 @@
 import sys
 import unicodedata
 
-word = None
-if len (sys.argv) > 1:
-    if sys.argv[1] in ("-h", "--help"):
-        print("usage: {0} [string]".format(sys.argv[0]))
-        word = 0
-    else:
-        word = sys.argv[1].lower()
-        if word != 0:
-            print_unicode_table(word)
+words = [None, 0, ' '.join(sys.argv[1::])]
 
-def print_unicode_table(word):
+def print_unicode_table(words):
+
     print("decimal  hex  chr  {0:^40}".format("name"))
     print("------- ----- ---  {0:-<40}".format(""))
 
@@ -21,6 +14,28 @@ def print_unicode_table(word):
     while code < end:
         c = chr(code)
         name = unicodedata.name(c, "*** unknown ***")
-        if word is None or word in name.lower():
-            print("{0:7} {0:5X} {0:^3c} {1}".format(code, name.title()))
-        code += 1 
+        if words.find(" ") == -1:
+            if words == words[0] or words in name.lower():
+                print("{0:7} {0:5X} {0:^3c} {1}".format(code, name.title()))
+        else:
+            j = words.split(" ")                #временный список
+            count = 0                           #Добавляем счетчик для сравнения его с длинной списка
+            for word in j:                      #Проходимся по элементам в списке
+                if word in name.lower():        #Если элемент из списка находится в строке name, то увеличиваем счетчик
+                    count += 1
+                    if count == len(j):         #Сравниваем счетчик с длинной временного списка и если они равны, то выводим строку name
+                        print("{0:7} {0:5X} {0:^3c} {1}".format(code, name.title()))
+
+        code += 1
+
+if len (sys.argv) > 1:
+    if sys.argv[1] in ("-h", "--help"):
+        print("usage: {0} [string]".format(sys.argv[0]))
+        words = words[1]
+    else:
+        words = words[2].lower()
+        if words != 0:
+            print_unicode_table(words)
+
+
+ 
